a769e625d6b35d43c70bd1eb1a46fa609d706bb0
jorgehara <empresamodelo2018@gmail.com>
2023-01-09 16:31:59 -0300

    deleteLayout

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,12 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import CardsProjects from "../components/CardsProjects";
-import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ name }) {
   name = "JorgeHara";
   return (
     <>
-      <Layout />
       {/* Header Card */}
       <Head className="container-fluid text-center">
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -68,7 +66,6 @@ export default function Home({ name }) {
             </div>
             </div>
       </div>
-      <Layout />
     </>
   );
 }
